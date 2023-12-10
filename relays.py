cb27d2886bfd06d6d95c52b02dc3aa3948251b66
arturogsalazar28 <agerardosn28@hotmail.com>
2023-12-10 17:32:31 -0600

    Adding basic functions to run more easily

---
+++ relays.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Pin Setup
+# Pin number   WiringPi Pin   BCM Pin  Relay
+#       3           8           2       1
+#       5           9           3       2
+#       7           7           4       3
+#       8           15          14      4
+#       10          16          15      5
+#       12          1           18      6
+#       11          0           17      7
+#       13          2           27      8
+
+relays = [2, 3, 4, 14, 15, 18, 17, 27]
+
+
+# Main function for the program
+def main(bit_stamp_array):
+
+    # Cleanup the GPIO
+    GPIO.cleanup()
+    # Setup GPIO for the relays
+    GPIO.setmode(GPIO.BCM)
+
+    # Setup all the relays to off
+    for relay in relays:
+        GPIO.setup(relay, GPIO.OUT)
+        GPIO.output(relay, 1)
+
+    # Based on the bitStampArray, switch the relays on and off starting with 1
+    for bitStamp in bit_stamp_array:
+        for relayNumber in range(8):
+            if int(bitStamp[0], 2) & (int('10000000', 2) >> relayNumber):
+                GPIO.output(relays[relayNumber], 0)
+            else:
+                GPIO.output(relays[relayNumber], 1)
+
+
+
+# init code that takes in a bitstamp from the command line
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python3 relays.py <bitstamp>")
+        sys.exit(1)
+    bit_stamp_array = sys.argv[1]
+    main(bit_stamp_array)
